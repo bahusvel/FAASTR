074f37ab4312bb098efa9aef2341abbe85a248d8
Denis Lavrov <bahus.vel@gmail.com>
2018-10-25 15:07:24 +1300

    Implemented quickcheck encode_decode

--- sos-rs/src/lib.rs
+++ sos-rs/src/lib.rs
@@ -57,6 +57,13 @@ impl<'a> From<Function<'a>> for OwnedFunction {
     }
 }
 
+#[cfg(feature = "alloc")]
+impl Debug for OwnedFunction {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{}::{}", self.module, self.name)
+    }
+}
+
 impl<'a> Debug for Function<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         write!(f, "{}::{}", self.module, self.name)
@@ -76,16 +83,52 @@ pub enum Value<'a> {
     UInt64(u64),
     Float(f32),
     Double(f64),
-    #[cfg(feature = "alloc")]
-    OwnedError(String),
     Error(&'a str),
-    #[cfg(feature = "alloc")]
-    OwnedString(String),
     String(&'a str),
     Opaque(&'a [u8]),
     Function(Function<'a>),
     EmbeddedOut(DecodeIter<'a>),
     EmbeddedIn(ReferencedValues<'a>),
+    #[cfg(feature = "alloc")]
+    EmbeddedVec(Vec<Value<'a>>),
+}
+
+#[cfg(feature = "alloc")]
+#[derive(Debug, Clone, PartialEq)]
+pub enum OwnedValue {
+    Int32(i32),
+    UInt32(u32),
+    Int64(i64),
+    UInt64(u64),
+    Float(f32),
+    Double(f64),
+    Error(String),
+    String(String),
+    Opaque(Vec<u8>),
+    Function(OwnedFunction),
+    Embedded(Vec<OwnedValue>),
+}
+
+#[cfg(feature = "alloc")]
+impl OwnedValue {
+    pub fn borrow(&self) -> Value {
+        match self {
+            OwnedValue::Int32(i) => Value::Int32(*i),
+            OwnedValue::UInt32(i) => Value::UInt32(*i),
+            OwnedValue::Int64(i) => Value::Int64(*i),
+            OwnedValue::UInt64(i) => Value::UInt64(*i),
+            OwnedValue::Float(i) => Value::Float(*i),
+            OwnedValue::Double(i) => Value::Double(*i),
+            OwnedValue::Error(i) => Value::Error(i),
+            OwnedValue::String(i) => Value::String(i),
+            OwnedValue::Opaque(i) => Value::Opaque(i),
+            OwnedValue::Function(i) => Value::Function(Function {
+                module: &i.module,
+                name: &i.name,
+            }),
+            OwnedValue::Embedded(i) => Value::EmbeddedVec(i.iter().map(|v| v.borrow()).collect()),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -147,7 +190,7 @@ impl<'a> Deref for EncodedValues<'a> {
 #[cfg(feature = "alloc")]
 impl<'a> EncodedValues<'a> {
     pub fn decode(&self) -> DecodeIter {
-        decode_sos(&self)
+        decode_sos(&self, true)
     }
     pub fn into_owned(self) -> OwnedEncodedValues {
         self.0.into_owned()
@@ -198,7 +241,7 @@ impl<'a> SOS for EncodedValues<'a> {
 }
 
 impl<'a> Value<'a> {
-    fn type_name(&self) -> &'static str {
+    pub fn type_name(&self) -> &'static str {
         match self {
             Value::Int32(_) => stringify!(Value::Int32),
             Value::UInt32(_) => stringify!(Value::UInt32),
@@ -207,30 +250,45 @@ impl<'a> Value<'a> {
             Value::Float(_) => stringify!(Value::Float),
             Value::Double(_) => stringify!(Value::Double),
             Value::String(_) => stringify!(Value::String),
-            #[cfg(feature = "alloc")]
-            Value::OwnedString(_) => stringify!(Value::OwnedString),
-            #[cfg(feature = "alloc")]
-            Value::OwnedError(_) => stringify!(Value::OwnedError),
             Value::Error(_) => stringify!(Value::Error),
             Value::Opaque(_) => stringify!(Value::Opaque),
             Value::Function(_) => stringify!(Value::Function),
             Value::EmbeddedOut(_) => stringify!(Value::EmbeddedOut),
             Value::EmbeddedIn(_) => stringify!(Value::EmbeddedIn),
+            #[cfg(feature = "alloc")]
+            Value::EmbeddedVec(_) => stringify!(Value::EmbeddedVec),
         }
     }
+    #[inline(always)]
+    fn ctype(&self) -> CType {
+        match self {
+            Value::Int32(_) => CType::Int32,
+            Value::UInt32(_) => CType::UInt32,
+            Value::Int64(_) => CType::Int64,
+            Value::UInt64(_) => CType::UInt64,
+            Value::Float(_) => CType::Float,
+            Value::Double(_) => CType::Double,
+            Value::String(_) => CType::String,
+            Value::Error(_) => CType::Error,
+            Value::Opaque(_) => CType::Opaque,
+            Value::Function(_) => CType::Function,
+            Value::EmbeddedOut(_) | Value::EmbeddedIn(_) => CType::Embedded,
+            #[cfg(feature = "alloc")]
+            Value::EmbeddedVec(_) => CType::Embedded,
+        }
+    }
+    #[inline(always)]
     fn encoded_size(&self) -> usize {
         match self {
             &Value::Int32(_) | &Value::UInt32(_) | &Value::Float(_) => 4,
             &Value::Int64(_) | &Value::UInt64(_) | &Value::Double(_) => 8,
             &Value::String(ref i) => i.len() + 1,
-            #[cfg(feature = "alloc")]
-            &Value::OwnedString(ref i) => i.len() + 1,
             &Value::Error(ref i) => i.len() + 1,
-            #[cfg(feature = "alloc")]
-            &Value::OwnedError(ref i) => i.len() + 1,
             &Value::Opaque(ref i) => i.len(),
             &Value::EmbeddedIn(ref i) => i.encoded_len(),
             &Value::EmbeddedOut(ref f) => f.buff.len(),
+            #[cfg(feature = "alloc")]
+            &Value::EmbeddedVec(ref i) => ReferencedValues(&i[..]).encoded_len(),
             &Value::Function(ref f) => f.module.len() + f.name.len() + 2,
         }
     }
@@ -375,82 +433,37 @@ fn encode_sos(buf: &mut [u8], values: &[Value]) -> usize {
     let mut coffset = 16;
 
     for value in values {
-        let length: u32;
-        let val_type;
+        let length = value.encoded_size();
+        let val_type = value.ctype();
 
         match value {
-            &Value::Int32(i) => {
-                length = 4;
-                val_type = CType::Int32;
-                NativeEndian::write_i32(&mut buf[coffset..], i)
-            }
-            &Value::UInt32(i) => {
-                length = 4;
-                val_type = CType::UInt32;
-                NativeEndian::write_u32(&mut buf[coffset..], i)
-            }
-            &Value::Int64(i) => {
-                length = 8;
-                val_type = CType::Int64;
-                NativeEndian::write_i64(&mut buf[coffset..], i)
-            }
-            &Value::UInt64(i) => {
-                length = 8;
-                val_type = CType::UInt64;
-                NativeEndian::write_u64(&mut buf[coffset..], i)
-            }
-            &Value::Float(i) => {
-                length = 4;
-                val_type = CType::Float;
-                NativeEndian::write_f32(&mut buf[coffset..], i)
-            }
-            &Value::Double(i) => {
-                length = 8;
-                val_type = CType::Double;
-                NativeEndian::write_f64(&mut buf[coffset..], i)
-            }
+            &Value::Int32(i) => NativeEndian::write_i32(&mut buf[coffset..], i),
+            &Value::UInt32(i) => NativeEndian::write_u32(&mut buf[coffset..], i),
+            &Value::Int64(i) => NativeEndian::write_i64(&mut buf[coffset..], i),
+            &Value::UInt64(i) => NativeEndian::write_u64(&mut buf[coffset..], i),
+            &Value::Float(i) => NativeEndian::write_f32(&mut buf[coffset..], i),
+            &Value::Double(i) => NativeEndian::write_f64(&mut buf[coffset..], i),
             &Value::String(i) => {
-                length = i.len() as u32 + 1;
-                val_type = CType::String;
-                (&mut buf[coffset..coffset + i.len()]).copy_from_slice(i.as_bytes());
-                buf[coffset + i.len()] = 0;
-            }
-            #[cfg(feature = "alloc")]
-            &Value::OwnedString(ref i) => {
-                length = i.len() as u32 + 1;
-                val_type = CType::String;
-                (&mut buf[coffset..coffset + i.len()]).copy_from_slice(i.as_bytes());
-                buf[coffset + i.len()] = 0;
-            }
-            #[cfg(feature = "alloc")]
-            &Value::OwnedError(ref i) => {
-                length = i.len() as u32 + 1;
-                val_type = CType::Error;
                 (&mut buf[coffset..coffset + i.len()]).copy_from_slice(i.as_bytes());
                 buf[coffset + i.len()] = 0;
             }
             &Value::Error(i) => {
-                length = i.len() as u32 + 1;
-                val_type = CType::Error;
                 (&mut buf[coffset..coffset + i.len()]).copy_from_slice(i.as_bytes());
                 buf[coffset + i.len()] = 0;
             }
             &Value::Opaque(i) => {
-                length = i.len() as u32;
-                val_type = CType::Opaque;
                 (&mut buf[coffset..coffset + i.len()]).copy_from_slice(&i);
             }
             &Value::EmbeddedIn(ref i) => {
-                val_type = CType::Embedded;
-                length = i.encode(&mut buf[coffset..]) as u32;
+                i.encode(&mut buf[coffset..]);
             }
             &Value::EmbeddedOut(ref f) => {
-                val_type = CType::Embedded;
-                length = f.encode(&mut buf[coffset..]) as u32;
+                f.encode(&mut buf[coffset..]);
+            }
+            &Value::EmbeddedVec(ref i) => {
+                ReferencedValues(&i[..]).encode(&mut buf[coffset..]);
             }
             &Value::Function(ref f) => {
-                length = (f.module.len() + f.name.len() + 2) as u32;
-                val_type = CType::Function;
                 (&mut buf[coffset..coffset + f.module.len()]).copy_from_slice(f.module.as_bytes());
                 buf[coffset + f.module.len()] = 0;
                 (&mut buf[coffset + 1 + f.module.len()..coffset + length as usize - 1])
@@ -459,7 +472,7 @@ fn encode_sos(buf: &mut [u8], values: &[Value]) -> usize {
             }
         }
         NativeEndian::write_u32(&mut buf[coffset - 8..coffset - 4], val_type as u32);
-        NativeEndian::write_u32(&mut buf[coffset - 4..coffset], length);
+        NativeEndian::write_u32(&mut buf[coffset - 4..coffset], length as u32);
         coffset += length as usize + 8
     }
     NativeEndian::write_u32(&mut buf[..4], values.len() as u32);
@@ -472,6 +485,7 @@ fn encode_sos(buf: &mut [u8], values: &[Value]) -> usize {
 pub struct DecodeIter<'a> {
     count: usize,
     buff: &'a [u8],
+    lazy: bool,
 }
 
 impl<'a> SOS for DecodeIter<'a> {
@@ -492,11 +506,12 @@ impl<'a> DecodeIter<'a> {
     }
 }
 
-pub fn decode_sos(buff: &[u8]) -> DecodeIter {
+pub fn decode_sos(buff: &[u8], lazy: bool) -> DecodeIter {
     let count = NativeEndian::read_u32(&buff[..4]) as usize;
     DecodeIter {
         count: count,
         buff: &buff[8..],
+        lazy: lazy,
     }
 }
 
@@ -504,12 +519,12 @@ impl<'a> Iterator for DecodeIter<'a> {
     type Item = Value<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.buff.len() <= 8 || self.count == 0 {
+        if self.buff.len() < 8 || self.count == 0 {
             return None;
         }
         let val_type = NativeEndian::read_u32(&self.buff[..4]);
         let val_length = NativeEndian::read_u32(&self.buff[4..8]) as usize;
-        assert!(val_length != 0);
+        //assert!(val_length != 0);
         assert!(val_length + 8 <= self.buff.len());
         let val_data = &self.buff[8..8 + val_length];
         let val = match CType::from_u32(val_type)? {
@@ -530,10 +545,14 @@ impl<'a> Iterator for DecodeIter<'a> {
                 let name = from_utf8(&vec[pos + 1..]).unwrap();
                 Value::Function(Function { module, name })
             }
-            CType::Embedded => Value::EmbeddedOut(DecodeIter {
-                count: NativeEndian::read_u32(&self.buff[..4]) as usize,
-                buff: &val_data[8..],
-            }),
+            CType::Embedded => {
+                let iter = decode_sos(val_data, self.lazy);
+                if self.lazy {
+                    Value::EmbeddedOut(iter)
+                } else {
+                    Value::EmbeddedVec(iter.collect::<Vec<_>>())
+                }
+            }
         };
         self.buff = &self.buff[val_length + 8..];
         self.count -= 1;
--- sos-rs/tests/test.rs
+++ sos-rs/tests/test.rs
@@ -1,12 +1,20 @@
+#![feature(plugin)]
+#![plugin(quickcheck_macros)]
+#![feature(custom_attribute)]
+//#[macro_use]
+extern crate quickcheck;
 extern crate rand;
 extern crate rand_xorshift;
 extern crate sos;
-use rand::distributions::Alphanumeric;
+use quickcheck::{Arbitrary, Gen};
+use rand::distributions::{Alphanumeric, Standard};
 use rand::prelude::*;
 use rand_xorshift::XorShiftRng;
 use sos::*;
+use std::fmt::{Debug, Formatter, Result};
 
 const MAX_LENGTH: usize = 1000;
+const MAX_EMBEDDED_SIZE: usize = 5;
 
 #[derive(Clone, Copy)]
 enum RngType {
@@ -22,7 +30,7 @@ enum RngType {
     Function,
     Embedded,
 }
-static TYPES: [RngType; 10] = [
+static TYPES: [RngType; 11] = [
     RngType::Int32,
     RngType::UInt32,
     RngType::Int64,
@@ -33,37 +41,56 @@ static TYPES: [RngType; 10] = [
     RngType::String,
     RngType::Opaque,
     RngType::Function,
+    RngType::Embedded,
 ];
+
+#[derive(Clone)]
+struct RngValue(OwnedValue);
+
+impl Debug for RngValue {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Arbitrary for RngValue {
+    fn arbitrary<G: Gen>(rng: &mut G) -> Self {
+        RngValue(gen_rand_type(*rng.choose(&TYPES[..]).unwrap(), rng))
+    }
+}
 
 fn gen_rand_string<R: Rng>(rng: &mut R) -> String {
     let length = rng.gen::<usize>() % MAX_LENGTH;
     rng.sample_iter(&Alphanumeric).take(length).collect()
 }
 
-fn gen_rand_type<R: Rng>(t: RngType, rng: &mut R) -> sos::Value<'static> {
+fn gen_rand_type<R: Rng>(t: RngType, rng: &mut R) -> OwnedValue {
     match t {
-        RngType::Int32 => sos::Value::Int32(rng.gen()),
-        RngType::UInt32 => sos::Value::UInt32(rng.gen()),
-        RngType::Int64 => sos::Value::Int64(rng.gen()),
-        RngType::UInt64 => sos::Value::UInt64(rng.gen()),
-        RngType::Float => sos::Value::Float(rng.gen()),
-        RngType::Double => sos::Value::Double(rng.gen()),
-        RngType::String => sos::Value::OwnedString(gen_rand_string(rng)),
-        RngType::Error => sos::Value::OwnedError(gen_rand_string(rng)),
-        _ => panic!("Not implemented"), /*
-    RngType::Opaque =>,
-    RngType::Function =>,
-    RngType::Embedded =>,
-    */
+        RngType::Int32 => OwnedValue::Int32(rng.gen()),
+        RngType::UInt32 => OwnedValue::UInt32(rng.gen()),
+        RngType::Int64 => OwnedValue::Int64(rng.gen()),
+        RngType::UInt64 => OwnedValue::UInt64(rng.gen()),
+        RngType::Float => OwnedValue::Float(rng.gen()),
+        RngType::Double => OwnedValue::Double(rng.gen()),
+        RngType::String => OwnedValue::String(gen_rand_string(rng)),
+        RngType::Error => OwnedValue::Error(gen_rand_string(rng)),
+        RngType::Opaque => {
+            let length = rng.gen::<usize>() % MAX_LENGTH;
+            OwnedValue::Opaque(rng.sample_iter(&Standard).take(length).collect())
+        }
+        RngType::Function => OwnedValue::Function(OwnedFunction {
+            module: gen_rand_string(rng),
+            name: gen_rand_string(rng),
+        }),
+        RngType::Embedded => OwnedValue::Embedded(gen_rand_sos(MAX_EMBEDDED_SIZE, rng)),
     }
 }
 
-fn gen_rand_sos<R: Rng>(num_values: usize, rng: &mut R) -> Vec<sos::Value<'static>> {
+fn gen_rand_sos<R: Rng>(num_values: usize, rng: &mut R) -> Vec<OwnedValue> {
     let mut vals = Vec::new();
     for _ in 0..num_values {
         vals.push(gen_rand_type(*rng.choose(&TYPES[..]).unwrap(), rng));
     }
-
     vals
 }
 
@@ -79,7 +106,43 @@ fn encode_decode() {
     ];
     let rvals = ReferencedValues(&vals);
     let len = rvals.encode(&mut buf[..]);
-    let decoded = decode_sos(&buf[..len]).collect::<Vec<_>>();
+    let decoded = decode_sos(&buf[..len], false).collect::<Vec<_>>();
     println!("{:?}", decoded);
     assert_eq!(rvals, ReferencedValues(&decoded[..]))
+}
+
+#[test]
+fn rand_encode_decode() {
+    let mut rng = XorShiftRng::seed_from_u64(10);
+    let vals = gen_rand_sos(MAX_EMBEDDED_SIZE, &mut rng);
+    println!("Encoding {:?}", vals);
+    let rvals = vals.iter().map(|v| v.borrow()).collect::<Vec<Value>>();
+    let refvals = ReferencedValues(&rvals[..]);
+    let length = refvals.encoded_len();
+    let mut buf = Vec::with_capacity(length);
+    unsafe {
+        buf.set_len(length);
+    }
+    refvals.encode(&mut buf[..]);
+    let decoded = decode_sos(&buf, false).collect::<Vec<_>>();
+    println!("Decoded {:?}", decoded);
+    assert_eq!(&rvals[..], &decoded[..]);
+}
+
+#[quickcheck]
+fn encode_decode_identity(vals: Vec<RngValue>) -> bool {
+    //println!("Encoding {:?}", vals);
+    let rvals = vals.iter().map(|v| v.0.borrow()).collect::<Vec<Value>>();
+    let refvals = ReferencedValues(&rvals[..]);
+    let length = refvals.encoded_len();
+    let mut buf = Vec::with_capacity(length);
+    unsafe {
+        buf.set_len(length);
+    }
+    refvals.encode(&mut buf[..]);
+    let decoded = decode_sos(&buf, false).collect::<Vec<_>>();
+    if &rvals[..] != &decoded[..] {
+        println!("Decoded {:?}\nArgs {:?}", decoded, rvals);
+    }
+    &rvals[..] == &decoded[..]
 }
